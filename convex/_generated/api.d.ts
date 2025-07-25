a2e358aa0dc75de6731c4f363c53e11191789c18
Yuu0413s <y.shibata0820@gmail.com>
2025-07-26 01:15:30 +0900

    🚧 npx installを再実行

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -13,11 +13,11 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
-import type * as f_data from "../f_data.js";
-import type * as p_data from "../p_data.js";
-import type * as t_data from "../t_data.js";
-import type * as t_video from "../t_video.js";
+import type * as personal from "../personal.js";
+import type * as recipe from "../recipe.js";
+import type * as t_menu from "../t_menu.js";
 import type * as users from "../users.js";
+import type * as w_training from "../w_training.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -28,11 +28,11 @@ import type * as users from "../users.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
-  f_data: typeof f_data;
-  p_data: typeof p_data;
-  t_data: typeof t_data;
-  t_video: typeof t_video;
+  personal: typeof personal;
+  recipe: typeof recipe;
+  t_menu: typeof t_menu;
   users: typeof users;
+  w_training: typeof w_training;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
