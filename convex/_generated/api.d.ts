abc29e516ae06586a5656e930457cc05ac022712
YzkSK <165975846+YzkSK@users.noreply.github.com>
2025-07-27 11:44:20 +0900

    パッケージ更新

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -21,6 +21,7 @@ import type * as recipe from "../recipe.js";
 import type * as sample_menu from "../sample_menu.js";
 import type * as t_list_manage from "../t_list_manage.js";
 import type * as t_menu from "../t_menu.js";
+import type * as training_log from "../training_log.js";
 import type * as training_records from "../training_records.js";
 import type * as users from "../users.js";
 import type * as w_training from "../w_training.js";
@@ -42,6 +43,7 @@ declare const fullApi: ApiFromModules<{
   sample_menu: typeof sample_menu;
   t_list_manage: typeof t_list_manage;
   t_menu: typeof t_menu;
+  training_log: typeof training_log;
   training_records: typeof training_records;
   users: typeof users;
   w_training: typeof w_training;
